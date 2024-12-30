7e378a3ef596526770157d79195e74bd0137a946
NicollMendez <nicoll.mendez339@comunidadunir.net>
2024-12-30 13:39:44 -0500

    cambios

--- src/components/Pay/pay.jsx
+++ src/components/Pay/pay.jsx
@@ -1,12 +1,9 @@
 import Search from "../Search/search.jsx";
 import PayEle from "./payele.jsx";
 import PayTotal from "./paytotal.jsx";
-import { useContext } from "react";
-import Context from "../../Context/context.jsx";
 import PayForm from "./payform.jsx";
 
 const Pay = () => {
-    const { car } = useContext(Context);
     return (
         <div>
             <Search />
