5675436d6e9e8400f6b6febdba341191f48a4f3d
NicollMendez <nicoll.mendez339@comunidadunir.net>
2024-12-30 13:44:09 -0500

    cambios

--- src/components/Car/car.jsx
+++ src/components/Car/car.jsx
@@ -3,7 +3,7 @@ import Search from "../Search/search";
 import CarEle from "./carele.jsx";
 import CarTotal from "./cartotal.jsx";
 import { useContext } from "react";
-import Context from "../../context/Context";
+import Context from "../../context/context.jsx";
 
 const Car = () => {
     const { car } = useContext(Context);
