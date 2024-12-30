8e333e0e848819f2114394bab21028f4ef0c32a7
NicollMendez <nicoll.mendez339@comunidadunir.net>
2024-12-30 13:36:40 -0500

    Cambios

--- src/components/Pay/pay.jsx
+++ src/components/Pay/pay.jsx
@@ -1,8 +1,8 @@
-import Search from "../Search/search";
+import Search from "../Search/search.jsx";
 import PayEle from "./payele.jsx";
 import PayTotal from "./paytotal.jsx";
 import { useContext } from "react";
-import Context from "../../Context/context";
+import Context from "../../Context/context.jsx";
 import PayForm from "./payform.jsx";
 
 const Pay = () => {
